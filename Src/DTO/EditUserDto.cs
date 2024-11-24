c3cbaf3f8da6d8980b6dcb548471fbbefa9d2a11
Dario Contreras <thedaroo81@gmail.com>
2024-11-23 22:04:15 -0300

    feat: add mi validation to editUserDto

--- Src/DTO/EditUserDto.cs
+++ Src/DTO/EditUserDto.cs
@@ -1,26 +1,24 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 using users_api.Src.Validators;
 
 namespace users_api.Src.DTO
 {
     public class EditUserDto
     {
-        
-        [StringLength(15, ErrorMessage = "El nombre del usuario no puede tener más de 15 caracteres")]
+        [MinLength(3, ErrorMessage = "El nombre del usuario debe tener al menos 3 caracteres")]
+        [MaxLength(15, ErrorMessage = "El nombre del usuario no puede tener más de 15 caracteres")]
         public string? Name { get; set; } = null;
 
-        [StringLength(100, ErrorMessage = "Los apellidos del usuario no pueden tener más de 100 caracteres")]
+        [MinLength(3, ErrorMessage = "El nombre del usuario debe tener al menos 3 caracteres")]
+        [MaxLength(100, ErrorMessage = "Los apellidos del usuario no pueden tener más de 100 caracteres")]
         public string? LastName { get; set; } = null;
 
         [OptionalEmail]  
-        [StringLength(100, ErrorMessage = "El correo electrónico del usuario no puede tener más de 100 caracteres")]
+        [MaxLength(100, ErrorMessage = "El correo electrónico del usuario no puede tener más de 100 caracteres")]
         public string? Email { get; set; } = null;
 
-        [StringLength(30, ErrorMessage = "El contraseña del usuario no puede tener más de 30 caracteres")]
+        [MinLength(3, ErrorMessage = "El nombre del usuario debe tener al menos 3 caracteres")]
+        [MaxLength(30, ErrorMessage = "El contraseña del usuario no puede tener más de 30 caracteres")]
         public string? Password { get; set; } = null;
 
         public bool? IsActive { get; set; } = null;
