0b9c014fafd885810f971c611c4a490801c06cf5
Elizabeth Garrido <105178087+elizabethG9@users.noreply.github.com>
2024-11-26 17:54:54 -0300

    feat: add Database.EnsureCreated

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ Env.Load();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite("Data Source=/app/data/EVSUM4.db"));
+builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite("Data Source=EVSUM4.db"));
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
@@ -46,7 +46,7 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var dbContext = services.GetRequiredService<DataContext>();
-
+    dbContext.Database.EnsureCreated();
     DataSeeder.Initialize(services);
 }
 
